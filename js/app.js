fb34eef0529abc5c7ba6bbb7370cb54fc2b904da
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-03-01 15:08:41 +0600

    error handling for the wrong search

--- js/app.js
+++ js/app.js
@@ -16,26 +16,36 @@ const searchPhone = async () => {
         const res = await fetch(url);
         const data = await res.json();
         displaySearchResult(data.data)
+        document.getElementById('not-search').style.display = "none";
     }
 
 }
 // display search result
 const displaySearchResult = phones => {
-    console.log(phones);
+    // console.log(phones);
     const searchResult = document.getElementById('search-result');
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
     searchResult.textContent = '';
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('phone-card')
-        div.innerHTML = `
-          <div" class="card h-100">
-                <img src="${phone.image}" class="p-3 card-img-top" alt="...">
-                <div class="card-body">
-                    <h5>${phone.phone_name}</h5>
-                    <h4>${phone.slug}</h4>
-                </div>
-           </div>
-        `
-        searchResult.appendChild(div);
-    });
+    if(phones == searchText){
+        document.getElementById('search-notFound').style.display = "block";
+    }
+    else{
+        phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('phone-card')
+            div.innerHTML = `
+              <div" class="card h-100">
+                    <img src="${phone.image}" class="p-3 card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5>${phone.phone_name}</h5>
+                        <h4>${phone.slug}</h4>
+                    </div>
+               </div>
+            `
+            searchResult.appendChild(div);
+            document.getElementById('search-notFound').style.display = "none";
+        });
+    }
+   
 }
