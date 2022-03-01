e563b33fa88e12c0a1236ddd66e825866636888d
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-03-01 13:20:37 +0600

    Error not searching message

--- js/app.js
+++ js/app.js
@@ -1,13 +1,41 @@
-// phone search handling 
+// phone search event 
 
-const searchPhone = async () =>{
+const searchPhone = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = ''
+    if (searchText == '') {
+        // Error not searching message
+        document.getElementById('not-search').style.display = "block";
+        const searchResult = document.getElementById('search-result');
+        searchResult.textContent = '';
+    }
+    else {
+        // load data
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySearchResult(data.data)
+    }
 
-// load data
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data.data);
+}
+// display search result
+const displaySearchResult = phones => {
+    console.log(phones);
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('phone-card')
+        div.innerHTML = `
+          <div" class="card h-100">
+                <img src="${phone.image}" class="p-3 card-img-top" alt="...">
+                <div class="card-body">
+                    <h5>${phone.phone_name}</h5>
+                    <h4>${phone.slug}</h4>
+                </div>
+           </div>
+        `
+        searchResult.appendChild(div);
+    });
 }
