27dcf23f6c69175d9db2629fa1f80ec1df22fd47
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-03-01 20:04:48 +0600

    detail  button and load phone detail

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const searchPhone = async () => {
         document.getElementById('not-search').style.display = "block";
         const searchResult = document.getElementById('search-result');
         searchResult.textContent = '';
+        document.getElementById('search-notFound').style.display = "none";
     }
     else {
         // load data
@@ -27,11 +28,12 @@ const displaySearchResult = phones => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchResult.textContent = '';
-    if(phones == searchText){
+    if (phones == searchText) {
         document.getElementById('search-notFound').style.display = "block";
     }
-    else{
+    else {
         phones.forEach(phone => {
+            // console.log(`${phone.slug}`);
             const div = document.createElement('div');
             div.classList.add('phone-card')
             div.innerHTML = `
@@ -39,13 +41,22 @@ const displaySearchResult = phones => {
                     <img src="${phone.image}" class="p-3 card-img-top" alt="...">
                     <div class="card-body">
                         <h5>${phone.phone_name}</h5>
-                        <h4>${phone.slug}</h4>
+                        <h4>${phone.brand}</h4>
                     </div>
+                    <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary p-2">Details</button>
+                    
                </div>
             `
             searchResult.appendChild(div);
             document.getElementById('search-notFound').style.display = "none";
         });
     }
-   
 }
+// load phone detail
+const loadPhoneDetail = async phoneId => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetail(data.data);
+};
+
